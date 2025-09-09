dfa44068df5d272081d9ad6b923394cacec233bd
David Dominguez <352985+ddominguez@users.noreply.github.com>
2025-09-08 20:29:54 -0400

    Moved func setupTestDB to utils_test

--- repository_test.go
+++ repository_test.go
@@ -5,34 +5,7 @@ import (
 	"fmt"
 	"testing"
 	"time"
-
-	"github.com/jmoiron/sqlx"
-	"github.com/pressly/goose/v3"
 )
-
-// setupTestDB creates an in-memory sqlite3 database and applies available migrations.
-func setupTestDB(t *testing.T) *sqlx.DB {
-	migrationsPath := "./migrations"
-
-	db, err := sqlx.Open("sqlite3", ":memory:")
-	if err != nil {
-		t.Fatalf("failed to open database: %v", err)
-	}
-
-	goose.SetLogger(goose.NopLogger())
-
-	err = goose.SetDialect(db.DriverName())
-	if err != nil {
-		t.Fatalf("failed to set dialect: %v", err)
-	}
-
-	err = goose.Up(db.DB, migrationsPath)
-	if err != nil {
-		t.Fatalf("failed to run migrations: %v", err)
-	}
-
-	return db
-}
 
 func TestHNStore_CreateStory(t *testing.T) {
 	db := setupTestDB(t)
